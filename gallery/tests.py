cf0bc2230ac2ac958991b1bf36dd6388784890c4
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-08 00:31:36 +0300

    Add test class method

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Images, Location, Category
+
 
 # Create your tests here.
+class ImagesTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.moses = Images(name = 'Moses',location='Nairobi',category= 'love')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moses,Images))
+
+    # Testing Save method
+    def test_save_method(self):
+        self.moses.save_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images) > 0)
