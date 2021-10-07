d01c3cf244a6307b3a668a54632a81b459205924
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-08 00:43:29 +0300

    Add class ArticleAdmin

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Images, Location, Category
 
 # Register your models here.
+class ImagesAdmin(admin.ModelAdmin):
+    filter_horizontal =('Category')
+
 admin.site.register(Images)
 admin.site.register(Location)
 admin.site.register(Category)
