e5c9d1b85ce3b7808e58a4bcbb4f29afc550e4d5
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-08 14:37:42 +0300

    Add search_results view function

--- gallery/views.py
+++ gallery/views.py
@@ -6,9 +6,21 @@ from .models import Images, Location, Category
 def welcome(request):
     return render(request,'welcome.html')
 
-
 def img(request):
     category = Category.get_category()
     images = Images.get_all_images()
     images_location = Location.get_location()
     return render(request,'images.html', {'category': category, 'images': images, 'images_location': images_location})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Images.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
