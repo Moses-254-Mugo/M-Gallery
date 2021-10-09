f55725f5fb1eb66303749fce48718c9641e527db
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-09 21:20:49 +0300

    Modify files

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.Home,name = 'Home'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^location/(\d+)', views.filter_by_location, name='location_image')
+    url(r'^location/(\d+)', views.search_by_location, name='search_location')
 ]
 
 
--- gallery/views.py
+++ gallery/views.py
@@ -3,14 +3,12 @@ from django.http  import HttpResponse
 from .models import Images, Location, Category
 
 # Create your views here.
-def welcome(request):
-    return render(request,'welcome.html')
 
 def Home(request):
     category = Category.get_category()
-    images = Images.get_all_images()
+    # images = Images.get_all_images()
     images_location = Location.get_location()
-    return render(request,'Home.html', {'category': category, 'images': images, 'images_location': images_location})
+    return render(request,'Home.html', {'category': category, 'images_location': images_location})
 
 def search_results(request):
 
@@ -28,3 +26,4 @@ def search_results(request):
 def search_by_location(request, location):
     location_image = Images.filter_by_location(location)
     return render(request,'location.html', {"location_image":location_image})
+    
