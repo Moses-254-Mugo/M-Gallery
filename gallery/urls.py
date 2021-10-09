fcfde5a811bc574f6b0998b093d60acd212141cd
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-09 14:58:04 +0300

    Add location url

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.Home,name = 'Home'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(\d+)', views.filter_by_location, name='location_image')
 ]
 
 
--- gallery/views.py
+++ gallery/views.py
@@ -24,3 +24,7 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
+    
+def search_by_location(request, location):
+    location_image = Images.filter_by_location(location)
+    return render(request,'location.html', {"location_image":location_image})
